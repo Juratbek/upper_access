53380caa4854a17e9689ec3f68038f4fc8d4f65f
juratbek <macbookpro@Juratbek-Mac-2.local>
2023-08-29 19:24:16 +0500

    6:adding google oauth icon

--- src/components/auth/GoogleSignIn/GoogleSignIn.tsx
+++ src/components/auth/GoogleSignIn/GoogleSignIn.tsx
@@ -6,13 +6,12 @@ import { IGoogleSignInProps } from './GoogleSignIn.types';
 export const GoogleSignIn: FC<IGoogleSignInProps> = (props) => {
   useEffect(() => {
     window.onload = function (): void {
-      // @ts-ignore
       google.accounts.id.initialize({
-        client_id: '764412195563-ksihv19p11m367utqkn6421j3k0n6nc3.apps.googleusercontent.com',
+        client_id: '764412195563-62aehnbr1lkrip1rh5rffijdhh4dm57f.apps.googleusercontent.com',
         callback: console.log,
       });
-      // @ts-ignore
       google.accounts.id.renderButton(document.getElementById(props.id), {
+        type: 'icon',
         theme: 'outline',
         size: 'large',
       });
@@ -23,7 +22,7 @@ export const GoogleSignIn: FC<IGoogleSignInProps> = (props) => {
     <Button
       id={props.id}
       type='button'
-      style={{ padding: 0, border: '1px solid red', overflow: 'hidden' }}
+      style={{ padding: 0, overflow: 'hidden' }}
       className={props.className}
     />
   );
