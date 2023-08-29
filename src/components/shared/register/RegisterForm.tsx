9472ef8daca40c3209c807a0c6ef44aec5a282aa
juratbek <macbookpro@Juratbek-Mac-2.local>
2023-08-29 13:32:03 +0500

    1:completing register form

--- src/components/shared/register/RegisterForm.tsx
+++ src/components/shared/register/RegisterForm.tsx
@@ -7,10 +7,7 @@ import { PasswordValidityLevel, UsernameValidityError } from '..';
 
 const { name, bio, login, password, email } = REGISTER_FORM_FIELDS;
 
-export const RegisterForm: FC<{ resetFormType: () => void; formType: 'login' | 'register' }> = ({
-  resetFormType,
-  formType,
-}) => {
+export const RegisterForm: FC<{ resetFormType: () => void }> = ({ resetFormType }) => {
   const [currentStep, setCurrentStep] = useState<number>(1);
   const {
     register,
@@ -23,14 +20,17 @@ export const RegisterForm: FC<{ resetFormType: () => void; formType: 'login' | '
   const incrementCurrentStep = useCallback(() => setCurrentStep((prev) => ++prev), []);
   const decrementCurrentStep = useCallback(() => setCurrentStep((prev) => --prev), []);
 
-  const submitHandler = useCallback((event: TSubmitFormEvent) => {
-    try {
-      reset();
-    } catch (e) {
-      console.log(e);
-    }
-    console.log('🚀 ~ file: LoginForm.tsx:17 ~ submitHandler ~ event:', event);
-  }, []);
+  const submitHandler = useCallback(
+    (event: TSubmitFormEvent) => {
+      try {
+        reset();
+      } catch (e) {
+        console.log(e);
+      }
+      console.log('🚀 ~ file: LoginForm.tsx:17 ~ submitHandler ~ event:', event);
+    },
+    [reset],
+  );
 
   const errorHandler = async (e: FieldErrors): Promise<void> => {
     console.log(e);
@@ -108,23 +108,6 @@ export const RegisterForm: FC<{ resetFormType: () => void; formType: 'login' | '
             />
             <Error error={errors['check-password']} />
           </div>
-          {/* <div className='mb-2'>
-          <Controller
-            control={control}
-            name={recaptcha.name}
-            rules={recaptcha.options}
-            render={({ field: { onChange } }): JSX.Element => (
-              <Recaptcha
-                className='mb-1 register'
-                siteKey={process.env.NEXT_PUBLIC_GOOGLE_SITE_KEY || ''}
-                onSuccess={onChange}
-                onExpired={(): void => onChange(null)}
-                ref={recaptchaRef}
-              />
-            )}
-          />
-          <Error error={errors[recaptcha.name]} />
-        </div> */}
         </div>
         <Button className='w-100 mb-2'>
           {currentStep === 2 ? "Ro'yxatdan o'tish" : 'Davom etish'}
@@ -134,11 +117,9 @@ export const RegisterForm: FC<{ resetFormType: () => void; formType: 'login' | '
             Ortga
           </Button>
         )}
-        {formType === 'register' && currentStep === 1 && (
-          <Button color='outline-dark' className='w-100' onClick={resetFormType}>
-            Ortga
-          </Button>
-        )}
+        <Button color='outline-dark' className='w-100' onClick={resetFormType}>
+          Ortga
+        </Button>
       </div>
     </form>
   );
