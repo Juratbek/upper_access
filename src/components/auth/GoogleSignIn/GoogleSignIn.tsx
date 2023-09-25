aed7b7c4bf0c85a30f599d84d4b5d58efd82cc1c
juratbek <macbookpro@Juratbek-Mac-2.local>
2023-09-25 18:23:19 +0500

    main:creating env variable for google client id

--- src/components/auth/GoogleSignIn/GoogleSignIn.tsx
+++ src/components/auth/GoogleSignIn/GoogleSignIn.tsx
@@ -22,7 +22,7 @@ export const GoogleSignIn: FC<IGoogleSignInProps> = (props) => {
   useEffect(() => {
     window.onload = function (): void {
       google.accounts.id.initialize({
-        client_id: '764412195563-62aehnbr1lkrip1rh5rffijdhh4dm57f.apps.googleusercontent.com',
+        client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
         callback: authSuccessHandler,
       });
       google.accounts.id.renderButton(document.getElementById(props.id), {
